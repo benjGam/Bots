12e539735a18333d8903effa0939c3100793748d
benjGam <mecopilive@gmail.com>
2025-03-23 18:49:08 +0100

    update

--- JS/MonkeyType/scalable-timed.js
+++ JS/MonkeyType/scalable-timed.js
@@ -1,38 +1,3 @@
-// Random timed bot
-
-typingBox = document.getElementById("words");
-words = [...document.getElementsByClassName("word")].map((wordDiv) =>
-  [...wordDiv.getElementsByTagName("letter")]
-    .map((letterElement) => letterElement.textContent)
-    .join("")
-);
-
-function getRand(max = 2000) {
-  return Math.floor(Math.random() * max);
-}
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function writeWord(word) {
-  word += " ";
-  word = [...word];
-  for (const letter of word) {
-    await sleep(getRand(70)).then(() =>
-      document.execCommand("insertText", false, letter)
-    );
-  }
-}
-
-(async function () {
-  for (const word of words) {
-    await sleep(200).then(async () => await writeWord(word));
-  }
-})();
-
-// Scalable version
-
 typingBox = document.getElementById("words");
 
 function sleep(ms) {
@@ -44,8 +9,7 @@ function getRand(max = 2000) {
 }
 
 async function writeWord(word) {
-  word = [...word];
-  for (const letter of word) {
+  for (const letter of word.split("")) {
     await sleep(getRand(70)).then(() =>
       document.execCommand("insertText", false, letter)
     );
